ba8196eee638d40f516edce97279ad467ec23f5a
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2021-07-05 18:29:16 -0400

    remove PostList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PostList from './components/Postlist'
 import Product from './components/Product'
 import Header from './components/Header'
 import Footer from './components/Footer'
