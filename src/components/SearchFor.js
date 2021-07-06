d8df6cd1d7950e189ab243648d26d3a73378572b
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2021-07-06 01:14:10 -0400

    addition of pagination to the SPA

--- src/components/SearchFor.js
+++ src/components/SearchFor.js
@@ -9,10 +9,19 @@ const SearchFor = ({result}) => {
         <div className="container">
             {ProductArray}
 
+            <nav aria-label="Pagination" className="pagination">
             <p id="pageNumber">
                 {/* {ProductArray.length}{(ProductArray.length === 1) ? `Product` : `products`} of {results.length} */}
                 {ProductArray.length} {(ProductArray.length === 1) ? `product` : `products`} of {result.length}
             </p>
+            <ol className="pages">
+          <li><a href="#" aria-label="Current Page, Page 1" aria-current="true">1</a></li>
+          <li><a href="#" aria-label="Page 2">2</a></li>
+          <li><a href="#" aria-label="Page 3">3</a></li>
+          <li><a href="#" aria-label="Page 4">4</a></li>
+          <li><a href="#" aria-label="Page 5">5</a></li>
+        </ol>
+        </nav>
         </div>
     )
 }
