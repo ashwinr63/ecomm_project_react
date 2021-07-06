d97a5fa6da041cb58d385eb42fe61723042b8ec8
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2021-07-05 22:27:18 -0400

    Adding search functions for each products

--- src/components/SearchFor.js
+++ src/components/SearchFor.js
@@ -1,1 +1,18 @@
 import React from 'react'
+import ProductList from './ProductList'
+
+const SearchFor = ({results}) => {
+    const ProductArray = results.slice(0,20).map((products) => <ProductList key={products.prod_name} data={products}/>)
+
+    return (
+        <div className="container">
+            {ProductArray}
+
+            <p id="product_numbers">
+                {ProductArray.length}{(ProductArray.length === 1) ? `products` : `product`} of {result.length}
+            </p>
+        </div>
+    )
+}
+
+export default SearchFor
