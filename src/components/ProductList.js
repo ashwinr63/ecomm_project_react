f8033bea519266beef48c806fd1ab1ca01a8996f
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2021-09-02 00:58:42 -0400

    Updating...

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -11,40 +11,41 @@ const ProductList = ({data}) => {
         
     const cartProduct = useContext(ProdContext)
     const addToCart = cartProduct.addCart
-  
+    // const [img, setImg] =useState(``)
+    // import (`..img/${prod_img}`).then((image) => setImg(image.default))
     return (
-            <article className="product">
-            <header>
-                <div className="product-list">
-                    <Link to={`/SingleProductPage/${prod_name}`}><Image src={prod_img} alt={prod_name} className="img-class"/></Link>
-                </div>
-                <h3>{prod_name}</h3>
-                <data value="39"><del>{old_price}</del> <ins>{prod_price}</ins></data>
-                <p>{prod_desc}<Link to={`/SingleProductPage/${prod_id}`}></Link></p>
-            <dl>
-            <dt>Rating</dt>
-            <dd>{prod_rate}</dd>
-            </dl>  
-            </header>
-            <form className="product-filter">
-            <fieldset className="field-list">
-                <legend>Colours</legend>
-                <ul>
-                    {prod_color}
-                </ul>
-            </fieldset>
-            <fieldset className="field-list">
-                <legend>Types</legend>
-                <ol>
-                    {prod_type}
-                </ol>
-            </fieldset>
-        </form>
-            <footer>
-            <button type="button" className="add-cart"><ShoppingCartRoundedIcon /></button>
-            <button type="button" className="add-fav"><FavoriteRoundedIcon /></button>
-            </footer>   
-            </article>
+                <article className="product">
+                <header>
+                    <div className="product-list">
+                        <Link to={`/SingleProductPage/${prod_name}`}><Image src={prod_img} alt={prod_name} className="img-class"/></Link>
+                    </div>
+                    <h3>{prod_name}</h3>
+                    <data value="39"><del>{old_price}</del> <ins>{prod_price}</ins></data>
+                    <p>{prod_desc}<Link to={`/SingleProductPage/${prod_name}`}></Link></p>
+                <dl>
+                <dt>Rating</dt>
+                <dd>{prod_rate}</dd>
+                </dl>  
+                </header>
+              <form className="product-filter">
+                <fieldset className="field-list">
+                    <legend>Colours</legend>
+                    <ul>
+                        {prod_color}
+                    </ul>
+                </fieldset>
+                <fieldset className="field-list">
+                    <legend>Types</legend>
+                    <ol>
+                        {prod_type}
+                    </ol>
+                </fieldset>
+            </form>
+                <footer>
+                <button type="button" className="add-cart"><ShoppingCartRoundedIcon /></button>
+                <button type="button" className="add-fav"><FavoriteRoundedIcon /></button>
+                </footer>   
+                </article>
             )
         
     }
